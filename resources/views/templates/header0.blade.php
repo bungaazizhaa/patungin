6fc104ae8f5bb206ea85836e2043847ebc0a0357
Bunga Azizha <bungaazizha33@gmail.com>
2021-10-15 20:52:27 +0700

    nyoba slicing

---
+++ resources/views/templates/header0.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+    <div class="topnav">
+            <a class="navbar-brand" href="#">
+                <div class="logo-image">
+                <img src="{{asset('assets/images/Logo-smal.png')}}" alt="" class="img-fluid">
+                </div>
+            </a>
+            <div class="topnav-right">
+                <a href="#landingpage">Beranda</a>
+                <a href="#landingpage">Tentang</a>
+                <a href="#landingpage">Cara kerja</a>
+                <a class="login-button" href="#login">Login</a>
+            </div>
+        </div>
+</nav>
+</div>
